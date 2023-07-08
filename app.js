8ea5cb94b8c29b4a60bc588a6e6d5586074c8448
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-08 20:43:22 +0700

    Buổi 3: CRUD with json-server

--- app.js
+++ app.js
@@ -1,32 +1,116 @@
-// import http from "http";
-
-// const app = http.createServer((req, res) => {
-//   if (req.url === "/" && req.method === "GET") {
-//     res.end(`<h1>Day la trang chu</h1>`);
-//   }
-//   if (req.url === "/about" && req.method === "GET") {
-//     res.end(`<h1>Day la trang about</h1>`);
-//   }
-//   if (req.url === "/products" && req.method === "GET") {
-//     res.end(`<h1>Day la trang san pham</h1>`);
-//   }
-// });
-
 import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
 const app = express();
+dotenv.config();
+
+const { PORT, API_URL } = process.env;
+// destructuring
+
+app.use(express.json());
 
 // Request product list
-app.get("/products", (req, res) => {
-  res.end(`<h1>Day la trang san pham cua hoangnm</h1>`);
+app.get("/products", async (req, res) => {
+  try {
+    const { data } = await axios.get(`${API_URL}/products`);
+    if (!data) {
+      return res.status(404).json({
+        message: "Products not found",
+      });
+    }
+    return res.status(200).json({
+      message: "Products successfully",
+      products: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
-// Request details product
-app.get("/products/:id", (req, res) => {
-  // method, url, params, body, query
-  console.log(req.params.id);
-  res.end(`<h1>Day la trang chi tiet san pham co id la: ${req.params.id}</h1>`);
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { data } = await axios.get(`${API_URL}/products/${req.params.id}`);
+    if (!data) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+    return res.status(200).json({
+      message: "Product successfully",
+      products: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
-app.listen(8088, () => {
-  console.log(`Server is running on port 8088`);
+app.post("/products", async (req, res) => {
+  try {
+    const { data } = await axios.post(`${API_URL}/products/`, req.body);
+    if (!data) {
+      return res.status(404).json({
+        message: "Create Product not successful",
+      });
+    }
+    return res.status(200).json({
+      message: "Create Product successful",
+      products: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { data } = await axios.put(
+      `${API_URL}/products/${req.params.id}`,
+      req.body
+    );
+    if (!data) {
+      return res.status(404).json({
+        message: "Update Product not successful",
+      });
+    }
+    return res.status(200).json({
+      message: "Update Product successful",
+      products: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { status } = await axios.delete(
+      `${API_URL}/products/${req.params.id}`
+    );
+
+    if (!status || status !== 200) {
+      return res.status(404).json({
+        message: "Delete product not successful",
+      });
+    }
+    return res.status(200).json({
+      message: "Delete product successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
