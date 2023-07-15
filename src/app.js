613e2ad34d8a3abb420405add1348bc739fdfa42
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-15 21:05:37 +0700

    add middleware cors

--- src/app.js
+++ src/app.js
@@ -2,22 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./routes";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
-const { PORT } = process.env;
-// destructuring
+const { PORT, DB_URI } = process.env;
 
+app.use(cors())
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/web69").then(() => {
+mongoose.connect(`${DB_URI}`).then(() => {
   console.log("Database connection established!")
 })
 
 app.use("/api", router);
-
-// categories, comments, feedback, about, users, ....
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
