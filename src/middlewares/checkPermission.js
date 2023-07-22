b9d74fee58c65cd1179f35a50394ee93958bc300
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-22 20:21:46 +0700

    Buổi 7: bổ sung check jwt error

--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -13,7 +13,31 @@ export const checkPermission = async (req, res, next) => {
         }
 
         // Bước 2: Giải mã token
-        const decoded =  jwt.verify(token, SECRET_CODE)
+        // const decoded =  jwt.verify(token, SECRET_CODE)
+
+        // Cách 2:
+        jwt.verify(token, SECRET_CODE, async (err, decoded) => {
+            if(err) {
+                if(err.name === "TokenExpiredError") {
+                    return res.status(401).json({
+                        message: "Token hết hạn!" || err.message
+                    })
+                }
+                if(err.name === "JsonWebTokenError") {
+                    return res.status(401).json({
+                        message: "Token lỗi!" || err.message
+                    })
+                }
+
+                if(err.name === "NotBeforeError") {
+                    return res.status(401).json({
+                        message: "Token không hoạt động!" || err.message
+                    })
+                }
+
+            }
+            const user = await User.findById(decoded._id)
+        })
         
         // Bước 3: tìm user dựa trên token.payload đã giải mã.
         const user = await User.findById(decoded._id)
