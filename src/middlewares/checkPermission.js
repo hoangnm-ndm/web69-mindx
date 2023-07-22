aa6bae9edaf7293c8c90d943231db807ed51ed09
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-22 20:46:05 +0700

    Buổi 7: config mongodb atlas

--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -7,37 +7,42 @@ const { SECRET_CODE} =  process.env
 export const checkPermission = async (req, res, next) => {
     try {
         // Bước 1:  Tìm kiếm token từ headers
-        const token = req.headers.authorization.split(" ")[1];
-        if(!token) {
+        const bearToken = req.headers.authorization
+        if(!bearToken) {
             throw new Error("Bạn chưa đăng nhập!")
         }
+        const token = req.headers.authorization.split(" ")[1];
+        if(!token) {
+            // Bear 
+            throw new Error("Token rỗng!")
+        }
 
-        // Bước 2: Giải mã token
-        // const decoded =  jwt.verify(token, SECRET_CODE)
+        // Bước 2: Giải mã token (ngắn gọn)
+        const decoded =  jwt.verify(token, SECRET_CODE)
 
-        // Cách 2:
-        jwt.verify(token, SECRET_CODE, async (err, decoded) => {
-            if(err) {
-                if(err.name === "TokenExpiredError") {
-                    return res.status(401).json({
-                        message: "Token hết hạn!" || err.message
-                    })
-                }
-                if(err.name === "JsonWebTokenError") {
-                    return res.status(401).json({
-                        message: "Token lỗi!" || err.message
-                    })
-                }
+        // Cách 2: chi tiết
+        // jwt.verify(token, SECRET_CODE, async (err, decoded) => {
+        //     if(err) {
+        //         if(err.name === "TokenExpiredError") {
+        //             return res.status(401).json({
+        //                 message: "Token hết hạn!" || err.message
+        //             })
+        //         }
+        //         if(err.name === "JsonWebTokenError") {
+        //             return res.status(401).json({
+        //                 message: "Token lỗi!" || err.message
+        //             })
+        //         }
 
-                if(err.name === "NotBeforeError") {
-                    return res.status(401).json({
-                        message: "Token không hoạt động!" || err.message
-                    })
-                }
+        //         if(err.name === "NotBeforeError") {
+        //             return res.status(401).json({
+        //                 message: "Token không hoạt động!" || err.message
+        //             })
+        //         }
 
-            }
-            const user = await User.findById(decoded._id)
-        })
+        //     }
+        //     const user = await User.findById(decoded._id)
+        // })
         
         // Bước 3: tìm user dựa trên token.payload đã giải mã.
         const user = await User.findById(decoded._id)
