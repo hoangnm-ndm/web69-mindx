689a55b87da8c2b2e6e2e1881e48389eef2659ef
hoangm.ndm <hoangm.ndm@gmail.com>
2023-07-25 17:57:57 +0700

    edit note

--- Online_8.md
+++ Online_8.md
@@ -1,7 +1,11 @@
 # Bước 1: Tạo model cho Category.
 # Bước 2: Tạo CRUD cho category
-# Bước 3: Thay đổi model của products để thêm categoryId vào model product.
-# Bước 4: Trong controller của products: populate products khi thêm, sửa product.
-# Bước 5: Trong model của category, thay đổi để có thể lấy được productId theo category.
-# Bước 6: Trong controller của category, get, getAll sao cho lấy được danh sách sản phẩm theo danh mục.
-# Bước 7: Xử lý trường hợp 1 category bị xoá, các product của category đó sẽ trở về category mặc định.
+# Bước 3: Customize lại model của product và category
+Thay đổi model của products để thêm categoryId vào model product,
+Trong model của category, thay đổi để có thể lấy được productId theo category.
+Khi thay đổi model, cũng cần thay đổi lại validator
+# Bước 4: Trong controller của products:
+- Update Category khi có 1 sản phẩm được thêm, sửa, xoá tương ứng
+- populate("categoryId"): Gọi ra category tương ứng với product All và product Detail 
+
+# Bước 5: Xử lý trường hợp 1 category bị xoá (null), các product của category đó sẽ trở về category mặc định.
--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -13,7 +13,6 @@ export const checkPermission = async (req, res, next) => {
         }
         const token = req.headers.authorization.split(" ")[1];
         if(!token) {
-            // Bear 
             throw new Error("Token rỗng!")
         }
 
@@ -25,43 +24,35 @@ export const checkPermission = async (req, res, next) => {
         //     if(err) {
         //         if(err.name === "TokenExpiredError") {
         //             return res.status(401).json({
-        //                 message: "Token hết hạn!" || err.message
+        //                 message: "Token hết hạn!"
         //             })
         //         }
         //         if(err.name === "JsonWebTokenError") {
         //             return res.status(401).json({
-        //                 message: "Token lỗi!" || err.message
+        //                 message:  "Token lỗi!"
         //             })
         //         }
 
         //         if(err.name === "NotBeforeError") {
         //             return res.status(401).json({
-        //                 message: "Token không hoạt động!" || err.message
+        //                 message: "Token không hoạt động!"
         //             })
         //         }
 
         //     }
         //     const user = await User.findById(decoded._id)
+        //     if(!user || user.role !== "admin") {
+        //         throw new Error("Bạn không có quyền làm việc này!")
+        //     }
         // })
         
         // Bước 3: tìm user dựa trên token.payload đã giải mã.
         const user = await User.findById(decoded._id)
 
         // Bước 4: Kiểm tra user và user.role
-        if(!user || user.role !== "admin") {
-            throw new Error("Bạn không có quyền làm việc này!")
+        if(!user) {
+            throw new Error("Tài khoản không tồn tại!")
         }
-
-        // if(!user) {
-        //     throw new Error("Tài khoản không tồn tại!")
-        // }
-
-        // if(user.role === "member"){
-
-        // } else if( user.role === "VIP")
-
-
-        // Sử dụng case switch để check quyền cho từng loại quyền.
 
         // Next:
         next()
